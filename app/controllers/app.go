7d28d6544616ab2709b28be1bc700d497e3c40fb
jiro4989 <jiroron666@gmail.com>
2019-07-23 20:46:44 +0900

    :sparkles: ひとまずチュートリアルが終わった

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -9,5 +9,21 @@ type App struct {
 }
 
 func (c App) Index() revel.Result {
-	return c.Render()
+	greeting := "こんにちは"
+	foobar := "fooooooooooo"
+	return c.Render(greeting, foobar)
+}
+
+func (c App) Hello(myName string) revel.Result {
+	c.Validation.Required(myName).Message("名前必須ですよ")
+	c.Validation.MinSize(myName, 3).Message("最低でも3文字は必要ですよ")
+
+	// バリデーションに問題があれば、Indexページにリダイレクトさせる
+	if c.Validation.HasErrors() {
+		c.Validation.Keep()
+		c.FlashParams()
+		return c.Redirect(App.Index)
+	}
+
+	return c.Render(myName)
 }
